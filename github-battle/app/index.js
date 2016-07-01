f3e83637a2ff90ebb5af8ea9efed02530509aa11
Shail Patel <shailpatel2@gmail.com>
2016-07-01 15:40:05 -0700

    More react fundamentals

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,15 +1,65 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var USER_DATA = {
+  name: 'Shail Patel',
+  username: 'shail',
+  image: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
+};
+
+var ProfilePic = React.createClass({
   render: function () {
-    return(
-      <div>Hello World</div>
+    return (<img src={this.props.imageUrl} style = {{height: 100, width: 100}} />)
+  }
+});
+
+var Link = React.createClass({
+  changeURL: function () {
+    window.location.replace(this.props.href)
+  },
+  render: function() {
+    return (
+      <span
+      style={{color: 'blue', cursor: 'pointer'}}
+      onClick={this.changeURL}>
+        {this.props.children}
+      </span>
     )
   }
-})
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
+    return(
+    <div>
+      <Link href ={'https://www.github.com/' + this.props.username}>
+        {this.props.username}
+      </Link>
+    </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function () {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+});
 
 ReactDOM.render(
-    <HelloWorld />,
-    document.getElementById('app')
+    <Avatar user={USER_DATA}/>, document.getElementById('app')
 )
